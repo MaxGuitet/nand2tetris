e62c67b972f3f46562e4e522bb1c4e53a0f5af32
Maxime <maxime.guitet@gmail.com>
2022-12-14 22:47:28 +0100

    Add brute force parsing of the COMP part of C-instruction

--- projects/06/assembler/hack/parse.asm
+++ projects/06/assembler/hack/parse.asm
@@ -71,7 +71,8 @@ M=D
 // R1 will store the assertion for "="
 // R2 will store the assertion for ";"
 // R3 will store the starting address of the instruction
-// R4 will store the output value of the instruction
+// R4 will store the sum for COMP
+// R5 will store the output value of the instruction
 (D_INS)
   @R0
   D=M
@@ -136,8 +137,8 @@ M=D
   // however 57344 is off-limit so instead we use 28672 = 57344/2 and add it twice
   @28672
   D=A
-  // Add it twice to R4
-  @R4
+  // Add it twice to R5
+  @R5
   M=D
   M=M+D
   // Reinitialize R0 to the begining of the instruction
@@ -159,17 +160,11 @@ M=D
   @PARSE_COMP
   0;JMP
 
-// We have a somehow limited number of character combination. 
-// Compute the sum and figure out which it is
-// A  => 65
-// D  => 68
-// M  => 77
-// AD => 133
-// AM => 142
-// DM => 145
-// ADM=> 210
+// We can have any combination of A,D and M.
+// Each is defined by a single bit in the DEST part
+// Loop and add all those which are present
 (PARSE_DEST)
-  // Check if = char (61)
+  // Check if "=" char (61)
   @61
   D=A
   @R0
@@ -177,7 +172,7 @@ M=D
   D=D-M
   @END_PARSE_DEST
   D;JEQ
-  // else get current char and add it to the sum in R4
+  // else get current char and add it to the sum in R5
   // check if A (65)
   @65
   D=A
@@ -208,7 +203,7 @@ M=D
   @32
   D=A
   // Compute the bit or with A Dest
-  @R4
+  @R5
   M=M|D
   // Read next char
   @R0
@@ -221,7 +216,7 @@ M=D
   @16
   D=A
   // Compute the bit or with D Dest
-  @R4
+  @R5
   M=M|D
   // Read next char
   @R0
@@ -234,7 +229,7 @@ M=D
   @8
   D=A
   // Compute the bit or with M Dest
-  @R4
+  @R5
   M=M|D
   // Read next char
   @R0
@@ -243,7 +238,7 @@ M=D
   0;JMP
  
 
-// We have finished parsing before =
+// We have finished parsing before "="
 // continue to see what is after
 (END_PARSE_DEST)
   @R0
@@ -255,26 +250,499 @@ M=D
   0;JMP
 
 // Parse comp until we see either a ; or a \n
-// Similarly to DEST, compute the sum
+// To know which computation to do, we get all characters
+// and sum their char codes with the following formula:
+// sum = 1 * char1 + 2 * char2 + 3 * char3
+// We do this to avoid redundancy in the sum
 (PARSE_COMP)
+  // Reset R4
+  @R4
+  M=0
+  @PARSE_COMP_1
+  0;JMP
+
+// The 1st char is necessarily not ; or \n
+(PARSE_COMP_1)
+  @R0
+  A=M
+  D=M
+  // Add it to R4
+  @R4
+  M=M+D
+  // Go to next char
+  @R0
+  M=M+1
+  // Not needed since it's below, but helps clarifying
+  @PARSE_COMP_2
+  0;JMP
+
+(PARSE_COMP_2)
   // Check if ; char
   @59
   D=A
   @R0
   A=M
   D=D-M
-  @END_PARSE_COMP
+  @EXTRACT_COMP
   D;JEQ
-  // check if \n
+  // check if \n => if so go to next instruction
   @128
   D=A
   @R0
   A=M
   D=D-M
-  @END_PARSE_COMP
+  @EXTRACT_COMP
   D;JEQ
+  // Else take the value and add it twice
+  @R0
+  A=M
+  D=M
+  // Add it to R4
+  @R4
+  M=M+D
+  M=M+D
+  // Go to next char
+  @R0
+  M=M+1
+  // Not needed since it's below, but helps clarifying
+  @PARSE_COMP_3
+  0;JMP
 
-// We have finished parsing before =
+(PARSE_COMP_3)
+  // Check if ; char
+  @59
+  D=A
+  @R0
+  A=M
+  D=D-M
+  @EXTRACT_COMP
+  D;JEQ
+  // check if \n => if so go to next instruction
+  @128
+  D=A
+  @R0
+  A=M
+  D=D-M
+  @EXTRACT_COMP
+  D;JEQ
+  // Else take the value and add it thrice
+  @R0
+  A=M
+  D=M
+  // Add it to R4
+  @R4
+  M=M+D
+  M=M+D
+  M=M+D
+  // We have now finished, extract value
+  // Not needed since it's below, but helps clarifying
+  @EXTRACT_COMP
+  0;JMP
+
+// Probably not the most efficient thing.
+// We're reading brute-force the sum and mapping it to the correct output
+(EXTRACT_COMP)
+  // 0 => 48
+  @R4
+  D=M
+  @48
+  D=D-A
+  @COMP_0
+  D;JEQ
+  // 1 => 49
+  @R4
+  D=M
+  @49
+  D=D-A
+  @COMP_1
+  D;JEQ
+  // -1 => 143
+  @R4
+  D=M
+  @143
+  D=D-A
+  @COMP_MIN_1
+  D;JEQ
+  // D => 68
+  @R4
+  D=M
+  @68
+  D=D-A
+  @COMP_D
+  D;JEQ
+  // A => 65
+  @R4
+  D=M
+  @65
+  D=D-A
+  @COMP_A
+  D;JEQ
+  // M => 77
+  @R4
+  D=M
+  @77
+  D=D-A
+  @COMP_M
+  D;JEQ
+  // !D => 169
+  @R4
+  D=M
+  @169
+  D=D-A
+  @COMP_NOT_D
+  D;JEQ
+  // !A => 163
+  @R4
+  D=M
+  @163
+  D=D-A
+  @COMP_NOT_A
+  D;JEQ
+  // !M => 187
+  @R4
+  D=M
+  @187
+  D=D-A
+  @COMP_NOT_M
+  D;JEQ
+  // -D => 181
+  @R4
+  D=M
+  @181
+  D=D-A
+  @COMP_NEG_D
+  D;JEQ
+  // -A => 175
+  @R4
+  D=M
+  @175
+  D=D-A
+  @COMP_NEG_A
+  D;JEQ
+  // -M => 199
+  @R4
+  D=M
+  @199
+  D=D-A
+  @COMP_NEG_M
+  D;JEQ
+  // D+1 => 301
+  @R4
+  D=M
+  @301
+  D=D-A
+  @COMP_D_PLUS_1
+  D;JEQ
+  // A+1 => 298
+  @R4
+  D=M
+  @298
+  D=D-A
+  @COMP_A_PLUS_1
+  D;JEQ
+  // M+1 => 310
+  @R4
+  D=M
+  @310
+  D=D-A
+  @COMP_M_PLUS_1
+  D;JEQ
+  // D-1 => 305
+  @R4
+  D=M
+  @305
+  D=D-A
+  @COMP_D_MINUS_1
+  D;JEQ
+  // A-1 => 302
+  @R4
+  D=M
+  @302
+  D=D-A
+  @COMP_A_MINUS_1
+  D;JEQ
+  // M-1 => 314
+  @R4
+  D=M
+  @314
+  D=D-A
+  @COMP_A_MINUS_1
+  D;JEQ
+  // D+A => 349
+  @R4
+  D=M
+  @349
+  D=D-A
+  @COMP_D_PLUS_A
+  D;JEQ
+  // D+M => 385
+  @R4
+  D=M
+  @385
+  D=D-A
+  @COMP_D_PLUS_M
+  D;JEQ
+  // D-A => 353
+  @R4
+  D=M
+  @353
+  D=D-A
+  @COMP_D_MINUS_A
+  D;JEQ
+  // D-M => 389
+  @R4
+  D=M
+  @389
+  D=D-A
+  @COMP_D_MINUS_A
+  D;JEQ
+  // A-D => 359
+  @R4
+  D=M
+  @359
+  D=D-A
+  @COMP_A_MINUS_D
+  D;JEQ
+  // M-D => 371
+  @R4
+  D=M
+  @371
+  D=D-A
+  @COMP_M_MINUS_D
+  D;JEQ
+  // D&A => 339
+  @R4
+  D=M
+  @339
+  D=D-A
+  @COMP_D_AND_A
+  D;JEQ
+  // D&M => 375
+  @R4
+  D=M
+  @375
+  D=D-A
+  @COMP_D_AND_M
+  D;JEQ
+  // D|A => 511
+  @R4
+  D=M
+  @511
+  D=D-A
+  @COMP_D_OR_A
+  D;JEQ
+  // D|M => 547
+  @R4
+  D=M
+  @547
+  D=D-A
+  @COMP_D_OR_M
+  D;JEQ
+  // If nothing matched, we have an invalid code, go to end
+  @END
+  0;JMP
+
+
+(COMP_0)
+  // Code needs to be (0)101010(000000) [2] => 2688 [10]
+  @2688
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_1)
+  // Code needs to be (0)111111(000000) [2] => 4032 [10]
+  @4032
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_MIN_1)
+  // Code needs to be (0)111010(000000) [2] => 3712 [10]
+  @3712
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D)
+  // Code needs to be (0)001100(000000) [2] => 768 [10]
+  @768
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_A)
+  // Code needs to be (0)110000(000000) [2] => 3072 [10]
+  @3072
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_M)
+  // Code needs to be (1)110000(000000) [2] => 7168 [10]
+  @7168
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NOT_D)
+  // Code needs to be (0)001101(000000) [2] => 832 [10]
+  @832
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NOT_A)
+  // Code needs to be (0)110001(000000) [2] => 3136 [10]
+  @3136
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NOT_M)
+  // Code needs to be (1)110001(000000) [2] => 7232 [10]
+  @7232
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NEG_D)
+  // Code needs to be (0)001111(000000) [2] => 960 [10]
+  @960
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NEG_A)
+  // Code needs to be (0)110011(000000) [2] => 3264 [10]
+  @3264
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_NEG_M)
+  // Code needs to be (1)110011(000000) [2] => 7360 [10]
+  @7360
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_PLUS_1)
+  // Code needs to be (0)011111(000000) [2] => 1984 [10]
+  @1984
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_A_PLUS_1)
+  // Code needs to be (0)110111(000000) [2] => 3520 [10]
+  @3520
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_M_PLUS_1)
+  // Code needs to be (1)110111(000000) [2] => 7616 [10]
+  @7616
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_MINUS_1)
+  // Code needs to be (0)001110(000000) [2] => 896 [10]
+  @896
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_A_MINUS_1)
+  // Code needs to be (0)110010(000000) [2] => 3200 [10]
+  @3200
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_M_MINUS_1)
+  // Code needs to be (1)110010(000000) [2] => 7296 [10]
+  @7296
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_PLUS_A)
+  // Code needs to be (0)000010(000000) [2] => 128 [10]
+  @128
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_PLUS_M)
+  // Code needs to be (1)000010(000000) [2] => 4224 [10]
+  @4224
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_MINUS_A)
+  // Code needs to be (0)010011(000000) [2] => 1216 [10]
+  @1216
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_MINUS_M)
+  // Code needs to be (1)010011(000000) [2] => 5312 [10]
+  @5312
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_A_MINUS_D)
+  // Code needs to be (0)000111(000000) [2] => 448 [10]
+  @448
+  D=A
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_M_MINUS_D)
+  // Code needs to be (1)000111(000000) [2] => 4544 [10]
+  @4544
+  D=A
+  @ADD_COMP_BITS
+
+(COMP_D_AND_A)
+  // Code needs to be (0)000000(000000) [2] => 0 [10]
+  D=0
+  @ADD_COMP_BITS
+  0;JMP
+
+(COMP_D_AND_M)
+  // Code needs to be (1)000000(000000) [2] => 4096 [10]
+  @4096
+  D=A
+  @ADD_COMP_BITS
+
+(COMP_D_OR_A)
+  // Code needs to be (0)010101(000000) [2] => 1344 [10]
+  @1344
+  D=A
+  @ADD_COMP_BITS
+
+(COMP_D_OR_M)
+  // Code needs to be (1)010101(000000) [2] => 5440 [10]
+  @5440
+  D=A
+  @ADD_COMP_BITS
+
+// The value we want to add is in D
+// Compute the logical OR
+(ADD_COMP_BITS)
+  @R5
+  M=M|D
+  @END_PARSE_COMP
+  0;JMP
+
+// We have finished parsing COMP
 // continue to see what is after
 (END_PARSE_COMP)
   @R0
@@ -285,6 +753,9 @@ M=D
   @PARSE_JUMP
   D;JEQ
   // If we do not have a ";", we are done, move to next instruction
+  // Skip the next char as it is a \n
+  @R0
+  M=M+1
   @READ
   0;JMP
 
