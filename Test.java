2950bc177be2513c760f18b1b28fb466e571c04e
Maxime <maxime.guitet@gmail.com>
2022-12-23 11:18:47 +0100

    Move assembler to projects root

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String test = "0;JMP // jump to next";
-
-        test = test.replace(" ", "").replaceAll("\\/\\/.*", "");
-        System.out.println(test);
-    }
-}
